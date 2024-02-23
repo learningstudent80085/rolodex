1c644103e8767dd5a528b043492af1ef9738a7da
Johann Cruz <jshcruz.gl@gmail.com>
2024-02-23 08:11:12 +0800

    added CRUD functions except retrieve function

--- dbfunc.py
+++ dbfunc.py
@@ -1,7 +1,55 @@
 import sqlite3
 
-conn = sqlite3.connect("sample_db")
+conn = sqlite3.connect("sample_db.db")
 
 cursor = conn.cursor()
 
+#cursor.execute("INSERT INTO contacts VALUES ('john','doe','123456789','john@doe.com');")
+#conn.commit()
+cursor.execute("SELECT rowid,* FROM contacts")
+conn.commit()
+
+data = cursor.fetchall()
+for entry in data:
+    print(entry)
 conn.close()
+
+def add_entry():
+    first = input("Enter first name \n")
+    last = input("Enter last name \n")
+    phone = input("Enter phone number \n")
+    email = input("Enter email \n")
+    conn = sqlite3.connect("sample_db.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO contacts VALUES (?,?,?,?)", (first,last,phone,email))
+    conn.commit()
+    conn.close()
+
+def search_entry():
+    
+    conn = sqlite3.connect("sample_db.db")
+    cursor = conn.cursor()
+    conn.close()
+
+def update_entry():
+    idnum = input("Enter ID number \n")
+    first = input("Enter first name \n")
+    last = input("Enter last name \n")
+    phone = input("Enter phone number \n")
+    email = input("Enter email \n")
+    conn = sqlite3.connect("sample_db.db")
+    cursor = conn.cursor()
+    cursor.execute("UPDATE contacts SET first=?, last=?, phone=?, email=? WHERE rowid=?",(first,last,phone,email,idnum))
+    conn.commit()
+    conn.close()
+
+def delete_entry():
+    idnum = input("Enter ID number")
+    conn = sqlite3.connect("sample_db.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM contacts WHERE rowid=?",(idnum))
+    conn.commit()
+    conn.close()
+    print("entry deleted")
+
+#delete_entry()
